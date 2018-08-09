6189066f5493d4043f9489d7e809826689a97181
Camille Le Bruchec <Camille.lebruchec@gmail.com>
2018-08-09 14:45:22 +0200

    models ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+lemon = Ingredient.create!(name: "lemon")
+cramberry = Ingredient.create!(name: "cramberry")
+cucumber = Ingredient.create!(name: "cucumber")
+
+mojito = Cocktail.create!(name: "Mojito")
+cosmopolitain = Cocktail.create!(name: "Cosmopolitain")
+gin_tonic = Cocktail.create!(name: "Gin Tonic")
+
+Dose.create!(description: "6cl", cocktail: mojito, ingredient: lemon)
+Dose.create!(description: "4cl", cocktail: cosmopolitain, ingredient: cramberry)
+Dose.create!(description: "2cl", cocktail: gin_tonic, ingredient: cucumber)
