a5582a476bc04918bcc228afb95bf1b73aab75e6
Camille Le Bruchec <Camille.lebruchec@gmail.com>
2018-08-10 14:14:57 +0200

    seed modif

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,21 @@ Dose.destroy_all
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-lemon = Ingredient.create!(name: "lemon")
-cramberry = Ingredient.create!(name: "cramberry")
-cucumber = Ingredient.create!(name: "cucumber")
+
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
 
 mojito = Cocktail.create!(name: "Mojito")
 cosmopolitain = Cocktail.create!(name: "Cosmopolitain")
 gin_tonic = Cocktail.create!(name: "Gin Tonic")
 
-Dose.create!(description: "6cl", cocktail: mojito, ingredient: lemon)
-Dose.create!(description: "4cl", cocktail: cosmopolitain, ingredient: cramberry)
-Dose.create!(description: "2cl", cocktail: gin_tonic, ingredient: cucumber)
+# Dose.create!(description: "6cl", cocktail: mojito, ingredient: lemon)
+# Dose.create!(description: "4cl", cocktail: cosmopolitain, ingredient: cramberry)
+# Dose.create!(description: "2cl", cocktail: gin_tonic, ingredient: cucumber)
